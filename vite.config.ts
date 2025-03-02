7e4236ed97a2c04b0e6ca352219e4f899212e8e3
ZenithX-1 <zenithx996@gmail.com>
2025-03-02 14:50:09 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  base: '/webpage1/', // 👈 Add this line (replace with your repo name)
+});
